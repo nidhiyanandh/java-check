1a529787a6e3992542be6e16534775fcd8d83425
nithiyananthan <nidhiyanandh@live.com>
2025-05-18 14:57:30 +0530

    added junit and log4j2.properties

--- src/main/java/org/example/thinkjava/Goodbye.java
+++ src/main/java/org/example/thinkjava/Goodbye.java
@@ -1,6 +1,7 @@
 package org.example.thinkjava;
 
 public class Goodbye {
+    //private static final Logger LOGGER = LogManager.getLogger();
     public static void G1() {
         System.out.print("Goodbye, ");
                 System.out.println("cruel world");
